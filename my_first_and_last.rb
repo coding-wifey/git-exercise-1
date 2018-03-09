4d184622a09a15dba8087ec6662ee545a4461ae9
Kate <kateyamson@gmail.com>
2018-03-09 11:20:00 +0800

    First Draft: Name and Email Address

---
+++ my_first_and_last.rb
@@ -0,0 +1,6 @@
+my_first_name = "Kathleen Criselle"
+my_last_name = "Reverente"
+email_address = "kateyamson@gmail.com"
+
+puts "I am #{my_first_name} #{my_last_name}"
+puts "You can contact me at #{email_address}"
